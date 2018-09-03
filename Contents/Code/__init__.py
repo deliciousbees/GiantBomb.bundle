b9c6757ddb4398f3a42f8f1f32ce9fea3ad0b74b
Dan Teasdale <dan+github@nogoblin.com>
2018-09-03 11:39:06 -0700

    reworked main menu flow to be more useful ([All Shows] as a category, then live content, then latest videos in chronological order)

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,8 +4,8 @@ API_KEY = '70d735e54938286d6d9142727877107ced20e5ff'
 ART = 'art-default.jpg'
 ICON = 'icon-default.jpg'
 
-@handler('/video/giantbomb', 'Unofficial Giant Bomb')
-def MainMenu():
+@handler('/video/unofficialgiantbomb', 'Unofficial Giant Bomb')
+def MainMenu(cat_id=None, query=None, offset=0):
 	oc = ObjectContainer()
 
 	# Live stream
@@ -14,6 +14,16 @@ def MainMenu():
 	except Ex.HTTPError as detail:
 		Log.Error("Could not load list of live streams (server returned HTTP %d)", detail.code)
 	else:
+		oc.add(
+			DirectoryObject(
+				key='/video/unofficialgiantbomb/shows',
+				title='[All Shows]',
+				summary='Explore Giant Bomb content, organized by show.',
+				thumb=R(ICON),
+				art=R(ART)
+			)
+		)
+
 		chats = response['results']
 
 		for chat in chats:
@@ -36,16 +46,42 @@ def MainMenu():
 					)
 				)
 
-		oc.add(
-			DirectoryObject(
-				key='/video/giantbomb/videos',
-				title='Latest',
-				summary='Watch the newest stuff.',
-				thumb=R(ICON),
-				art=R(ART)
-			)
-		)
 
+
+	try:
+		result = JSON.ObjectFromURL('%s/videos/?api_key=%s&format=json&offset=0' % (API_PATH, ApiKey()))
+	except Ex.HTTPError as detail:
+		Log.Error("Could not load list of latest videos (server returned HTTP %d)", detail.code)
+	else:
+		videos = result['results']
+
+		for vid in videos:
+			oc.add(
+				VideoClipObject(
+					url=vid['api_detail_url'] + '?api_key=' + ApiKey() + '&format=json',
+					title=vid['name'],
+					summary=vid['deck'],
+					thumb=vid['image']['super_url'],
+					art=R(ART),
+					rating_key=vid['id']
+				)
+			)
+
+		listedSoFar = result['offset'] + result['number_of_page_results']
+		if listedSoFar < result['number_of_total_results']:
+			oc.add(
+				NextPageObject(
+					key=Callback(Videos, cat_id=cat_id, query=query, offset=listedSoFar),
+					title="Next Page"
+				)
+			)
+
+	return oc
+
+
+@route('/video/unofficialgiantbomb/shows', allow_sync=True)
+def AllShows():
+	oc = ObjectContainer()
 	try:
 		categories = JSON.ObjectFromURL(API_PATH + '/video_types/?api_key=' + ApiKey() + '&format=json')['results']
 	except Ex.HTTPError as detail:
@@ -54,9 +90,9 @@ def MainMenu():
 		for cat in categories:
 			# Endurance Runs
 			if cat['id'] == 5:
-				route = '/video/giantbomb/erun'
+				route = '/video/unofficialgiantbomb/erun'
 			else:
-				route = '/video/giantbomb/videos/?cat_id=' + str(cat['id'])
+				route = '/video/unofficialgiantbomb/videos/?cat_id=' + str(cat['id'])
 
 			oc.add(
 				DirectoryObject(
@@ -86,47 +122,47 @@ def MainMenu():
 
 	return oc
 
-@route('/video/giantbomb/erun', allow_sync=True)
+@route('/video/unofficialgiantbomb/erun', allow_sync=True)
 def EnduranceRunMenu():
 	oc = ObjectContainer(
 		objects = [
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=5&query=Chrono%20Trigger',
+				key='/video/unofficialgiantbomb/videos/?cat_id=5&query=Chrono%20Trigger',
 				title='Chrono Trigger',
 				art=R(ART)
 			),
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=5&query=Deadly%20Premonition',
+				key='/video/unofficialgiantbomb/videos/?cat_id=5&query=Deadly%20Premonition',
 				title='Deadly Premonition',
 				art=R(ART)
 			),
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=5&query=Persona%204',
+				key='/video/unofficialgiantbomb/videos/?cat_id=5&query=Persona%204',
 				title='Persona 4',
 				art=R(ART)
 			),
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=5&query=Matrix%20Online',
+				key='/video/unofficialgiantbomb/videos/?cat_id=5&query=Matrix%20Online',
 				title='The Matrix Online: Not Like This',
 				art=R(ART)
 			),
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=10&query=Breaking%20Brad%3A%20Demon%27s%20Souls',
+				key='/video/unofficialgiantbomb/videos/?cat_id=10&query=Breaking%20Brad%3A%20Demon%27s%20Souls',
 				title="Breaking Brad: Demon's Souls (Premium)",
 				art=R(ART)
 			),
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=10&query=Load%20Our%20Last%20Souls',
+				key='/video/unofficialgiantbomb/videos/?cat_id=10&query=Load%20Our%20Last%20Souls',
 				title="Load Our Last Souls (Premium)",
 				art=R(ART)
 			),
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=10&query=Metal%20Gear%20Scanlon',
+				key='/video/unofficialgiantbomb/videos/?cat_id=10&query=Metal%20Gear%20Scanlon',
 				title="Metal Gear Scanlon (Premium)",
 				art=R(ART)
 			),
 			DirectoryObject(
-				key='/video/giantbomb/videos/?cat_id=10&query=Bradley%20May%20Cry',
+				key='/video/unofficialgiantbomb/videos/?cat_id=10&query=Bradley%20May%20Cry',
 				title="Bradley May Cry (Premium)",
 				art=R(ART)
 			)
@@ -135,7 +171,7 @@ def EnduranceRunMenu():
 
 	return oc
 
-@route('/video/giantbomb/videos', allow_sync=True)
+@route('/video/unofficialgiantbomb/videos', allow_sync=True)
 def Videos(cat_id=None, query=None, offset=0):
 	oc = ObjectContainer()
 
